bd7ed6e13765ed587c95478e7edb2727608fa4dc
LenoraM <94599867+LenoraM@users.noreply.github.com>
2021-12-06 03:57:21 -0500

    Databinding on add customer page

---
+++ PROG8050-PROJECT/Models/Customer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG8050_PROJECT.Models
+{
+    class Customer:Notifier
+    {
+		private string email;
+		public string Email
+		{
+			get => email;
+			set
+			{
+				email = value;
+				OnPropertyChanged("Email");
+			}
+		}
+
+		private string name;
+		public string Name
+		{
+			get => name;
+			set
+			{
+				name = value;
+				OnPropertyChanged("Name");
+			}
+		}
+
+		private int gender;
+		public int Gender
+		{
+			get => gender;
+			set
+			{
+				gender = value;
+				OnPropertyChanged("Gender");
+			}
+		}
+
+		private string phoneNo;
+		public string PhoneNo
+		{
+			get => phoneNo;
+			set
+			{
+				phoneNo = value;
+				OnPropertyChanged("PhoneNo");
+			}
+		}
+	}
+}
---
+++ PROG8050-PROJECT/ViewModels/NewCustomerViewModel.cs
@@ -0,0 +1,90 @@
+﻿using PROG8050_PROJECT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace PROG8050_PROJECT.ViewModels
+{
+   class NewCustomerViewModel:Notifier
+    {
+		private Customer customer = new Customer();
+		const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		public Customer Customer
+		{
+			get => customer;
+			set
+			{
+				customer = value;
+				OnPropertyChanged("Customer");
+			}
+		}
+
+		private ICommand addCustomer;
+		public ICommand AddCustomer => (this.addCustomer) ??= new RelayCommand(parameter =>
+		{
+			if (String.IsNullOrEmpty(customer.Email))
+			{
+				System.Windows.MessageBox.Show("Please Enter the user email");
+				return;
+			}
+			if (String.IsNullOrEmpty(customer.Name))
+			{
+				System.Windows.MessageBox.Show("Please Enter the Name");
+				return;
+			}
+			if (String.IsNullOrEmpty(customer.Gender.ToString()))
+			{
+				System.Windows.MessageBox.Show("Please select a Gender");
+				return;
+			}
+			if (String.IsNullOrEmpty(customer.PhoneNo))
+			{
+				System.Windows.MessageBox.Show("Please Enter the user phoneNo");
+				return;
+			}
+
+
+			SQLiteDBManager dbManager = SQLiteDBManager.Instance;
+
+			if (!dbManager.IsOpen())
+			{
+				System.Windows.MessageBox.Show("Database is not connected");
+			}
+
+			Dictionary<string, object> param = new Dictionary<string, object>();
+			param.Add("@email", customer.Email);
+			param.Add("@name", customer.Name);
+			param.Add("@gender", customer.Gender);
+			param.Add("@phoneNo", customer.PhoneNo);
+			param.Add("@password", new string(Enumerable.Repeat(chars, 8)
+											.Select(s => s[new Random().Next(s.Length)]).ToArray()));
+
+
+			List<Customer> tempUser = dbManager.ExecuteReader<Customer>("select Email from user where email = @email", param);
+
+			//List<Customer> tempUser = dbManager.ExecuteReader<Customer>("select Email, name, gender, phone from user where email = @email", param);
+			if (tempUser.Count() > 0)
+			{
+				System.Windows.MessageBox.Show($"User @email already exists!");
+			}
+			else
+			{
+				int result = dbManager.ExecuteNonQuery("insert into user (email, Password, Name, Gender, Phone) values(@email,@password,@name,@gender,@phoneNo)", param);
+				if (result == 1)
+				{
+					System.Windows.MessageBox.Show($"User added successfully");
+				}
+				else
+				{
+					System.Windows.MessageBox.Show($"User addition failed");
+					return;
+				}
+			}
+		});
+
+
+	}
+}
