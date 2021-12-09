73961049e287a96e8a0688da4ab6c8480a1cff1a
ashithasufiyan <chikku7595@gmail.com>
2021-12-09 11:16:38 -0500

    updated Category and Product

---
+++ PROG8050-PROJECT/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG8050_PROJECT.Models
+{
+	class Category
+	{
+
+		public int Id { get; set; }
+		public int CategoryName { get; set; }
+
+
+
+		public override string ToString()
+		{
+			return $"{CategoryName}";
+		}
+
+	}
+}
--- PROG8050-PROJECT/Views/Category.xaml.cs
+++ PROG8050-PROJECT/Views/Category.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,29 +13,141 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Controls.Primitives;
+using System.Configuration;
+using System.Diagnostics;
 
+using System.Data;
+using System.Data.SQLite;
+using System.Data.SqlClient;
+using System.Collections;
+using System.ComponentModel;
 namespace PROG8050_PROJECT.Views
 {
-	/// <summary>
-	/// Interaction logic for Category.xaml
-	/// </summary>
-	public partial class Category : Page
-	{
-		public Category()
-		{
-			InitializeComponent();
-		}
+    /// <summary>
+    /// Interaction logic for category.xaml
+    /// </summary>
+    public partial class Category : Page
+    {
 
-		private void btnAddCatModal_Click(object sender, RoutedEventArgs e)
-		{
-			CategoryAddModal cataddmodal = new CategoryAddModal();
-			cataddmodal.ShowDialog();
-		}
+        SQLiteDataAdapter mAdapter;
+        DataTable dtable;
 
-		private void btnUpdateCategoryModal_Click(object sender, RoutedEventArgs e)
-		{
-			CatUpdate update = new CatUpdate();
-			update.ShowDialog();
-		}
-	}
+        string editcategoryid, editcategoryname;
+        object item;
+        public Category()
+        {
+            InitializeComponent();
+            FillDataGrid();
+        }
+
+
+        private void Button_Add_CategoryElement_Click(object sender, RoutedEventArgs e)
+        {
+            AddCategoryElementInputBox.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void Button_Edit_CategoryElement_Click(object sender, RoutedEventArgs e)
+        {
+            InputEditCategoryIdBox.Text = editcategoryid;
+            InputEditCategoryBox.Text = editcategoryname;
+            EditCategoryElementInputBox.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void categoryDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                if (this.categoryDataGrid.SelectedItem != null)
+                {
+                    item = this.categoryDataGrid.SelectedItem;
+                    editcategoryid = (this.categoryDataGrid.SelectedCells[0].Column.GetCellContent(item) as TextBlock).Text;
+                    this.InputEditCategoryIdBox.Text = editcategoryid;
+                    editcategoryname = (this.categoryDataGrid.SelectedCells[1].Column.GetCellContent(item) as TextBlock).Text;
+                    this.InputEditCategoryBox.Text = editcategoryname;
+                }
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show("Message:" + exp);
+            }
+        }
+        //
+        private void Button_Close_CategoryElement_Click(object sender, RoutedEventArgs e)
+        {
+            AddCategoryElementInputBox.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void Button_Submit_CategoryElement_Click(object sender, RoutedEventArgs e)
+        {
+            SQLiteDBManager dbManager = SQLiteDBManager.Instance;
+            var conn = dbManager.Connection;
+            using var cmd = new SQLiteCommand(conn);
+
+            cmd.CommandText = "Insert into Category (CategoryName)VALUES('" + this.InputCategoryBox.Text.ToString() + "')";
+            cmd.ExecuteNonQuery();
+            FillDataGrid();
+
+            //  conn.Close();
+            AddCategoryElementInputBox.Visibility = System.Windows.Visibility.Hidden;
+            MessageBox.Show($"{InputCategoryBox.Text} Category is succesfully inserted", "Success",
+             MessageBoxButton.OK);
+
+
+
+        }
+
+        private void EditCategoryUpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            SQLiteDBManager dbManager = SQLiteDBManager.Instance;
+            var conn = dbManager.Connection;
+
+            using (var cmd = new SQLiteCommand(conn))
+            {
+                conn.Open();
+                cmd.CommandText = "update Category SET CategoryName = '" + this.InputEditCategoryBox.Text.ToString()
+                  + "' where Id =" + Int32.Parse(this.InputEditCategoryIdBox.Text.ToString());
+                cmd.ExecuteNonQuery();
+                FillDataGrid();
+                conn.Close();
+                EditCategoryElementInputBox.Visibility = System.Windows.Visibility.Hidden;
+
+            }
+            //  
+            MessageBox.Show($"{InputEditCategoryBox.Text} Product is succesfully Updated", "Success",
+          MessageBoxButton.OK);
+
+        }
+
+        private void EditCategoryCancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            EditCategoryElementInputBox.Visibility = System.Windows.Visibility.Hidden;
+
+        }
+        private void FillDataGrid()
+        {
+            SQLiteDBManager dbManager = SQLiteDBManager.Instance;
+            var conn = dbManager.Connection;
+            using var cmd = new SQLiteCommand(conn);
+            cmd.CommandText = "select* from Category order by id";
+            cmd.ExecuteNonQuery();
+            mAdapter = new SQLiteDataAdapter(cmd);
+            dtable = new DataTable("Category");
+            mAdapter.Fill(dtable);
+            categoryDataGrid.ItemsSource = dtable.AsDataView();
+            mAdapter.Update(dtable);
+
+            this.categoryDataGrid.Columns[0].Header = "Id";
+            this.categoryDataGrid.Columns[1].Header = "Category";
+            //    
+
+
+
+
+
+
+
+        }
+    }
+
 }
